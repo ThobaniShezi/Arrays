59736ca109a3ebfe8a5a47f9d4cc9ddfd2c48741
ThobaniShezi <72035234+ThobaniShezi@users.noreply.github.com>
2021-10-11 09:27:46 +0200

    Add files via upload

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] evenNums = { 2, 4, 6, 8, 10 };
+
+            for (int i = 0; i < evenNums.Length; i++)
+                Console.WriteLine(evenNums[i]);
+
+            for (int i = 0; i < evenNums.Length; i++)
+                evenNums[i] = evenNums[i] + 10;
+
+            Console.ReadKey();
+        }
+    }
+}
